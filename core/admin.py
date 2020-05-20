2882db9015f0ad48081602840c229b14f6411ba3
Bruno Santos <brsantos88@yahoo.com.br>
2020-05-19 23:35:14 -0300

    criado tabela de eventos para trabalhar com a agenda

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from core.models import Evento
 
 # Register your models here.
+
+class EventoAdmin(admin.ModelAdmin):
+    list_display = ('titulo', 'data_evento', 'data_criacao', 'usuario', 'descricao')
+    list_filter = ('usuario', 'data_evento',)
+
+
+admin.site.register(Evento, EventoAdmin)
